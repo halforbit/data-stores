3dccd46f2ad7c5cbc32f015c6c53d001aa5bb387
Jim Noble <none@none>
2017-10-31 11:57:09 -0400

    fix connectionstring facet constructor

--- DocumentStores/PostgresMarten/Halforbit.DataStores.DocumentStores.PostgresMarten/Facets/ConnectionStringAttribute.cs
+++ DocumentStores/PostgresMarten/Halforbit.DataStores.DocumentStores.PostgresMarten/Facets/ConnectionStringAttribute.cs
@@ -5,6 +5,9 @@ namespace Halforbit.DataStores.DocumentStores.PostgresMarten.Facets
 {
     public class ConnectionStringAttribute : FacetParameterAttribute
     {
+        public ConnectionStringAttribute(string value = null, string configKey = null) : 
+            base(value, configKey) { }
+
         public override string ParameterName => "connectionString";
 
         public override Type TargetType => typeof(PostgresMartenDataStore<,>);
