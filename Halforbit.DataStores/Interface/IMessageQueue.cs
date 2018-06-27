541bcdf64cc8a517f21f32c41d14d61595237b6a
Jim Noble <none@none>
2018-06-27 18:49:22 -0400

    add IMessageQueue<> and Message<>

---
+++ Halforbit.DataStores/Interface/IMessageQueue.cs
@@ -0,0 +1,13 @@
+﻿using Halforbit.DataStores.Model;
+
+namespace Halforbit.DataStores.Interface
+{
+    public interface IMessageQueue<TContent>
+    {
+        Message<TContent> Get();
+
+        Message<TContent> Put(TContent content);
+
+        void Delete(Message<TContent> message);
+    }
+}
---
+++ Halforbit.DataStores/Model/Message.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Halforbit.DataStores.Model
+{
+    public class Message<TContent>
+    {
+        public Message(
+            string id,
+            string receipt,
+            TContent content)
+        {
+            Id = id;
+            Receipt = receipt;
+            Content = content;
+        }
+
+        public string Id { get; }
+
+        public string Receipt { get; }
+
+        public TContent Content { get; }
+
+        public static implicit operator TContent(Message<TContent> m)
+        {
+            return m.Content;
+        }
+    }
+}
