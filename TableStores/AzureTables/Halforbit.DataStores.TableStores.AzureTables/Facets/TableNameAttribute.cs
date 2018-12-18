0441814166ab3b6be6b41c36017a74df6d8aee03
Ryan O'Shaughnessy <ryan.oshag@gatech.edu>
2018-12-18 16:22:01 -0500

    Azure TableStorage integration

---
+++ TableStores/AzureTables/Halforbit.DataStores.TableStores.AzureTables/Facets/TableNameAttribute.cs
@@ -0,0 +1,15 @@
+﻿using Halforbit.DataStores.TableStores.AzureTables.Implementation;
+using Halforbit.Facets.Attributes;
+using System;
+
+namespace Halforbit.DataStores.TableStores.AzureTables.Facets
+{
+    public class TableNameAttribute : FacetParameterAttribute
+    {
+        public TableNameAttribute(string value = null, string configKey = null) : base(value, configKey) { }
+
+        public override Type TargetType => typeof(AzureTableStore<,>);
+
+        public override string ParameterName => "tableName";
+   } 
+}
