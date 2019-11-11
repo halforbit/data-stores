9ed0b4864c673111eb650c79da7a3ff394cb3950
Jim Noble <jimnoble@gmail.com>
2019-11-11 15:18:03 -0500

    Use camelCase and string enums with YAML serialization

--- FileStores/Serialization/Yaml/Halforbit.DataStores.FileStores.Serialization.Yaml/Implementation/YamlSerializer.cs
+++ FileStores/Serialization/Yaml/Halforbit.DataStores.FileStores.Serialization.Yaml/Implementation/YamlSerializer.cs
@@ -1,5 +1,7 @@
-﻿using Halforbit.DataStores.FileStores.Serialization.Json.Implementation;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
 using System.Dynamic;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,22 +12,36 @@ namespace Halforbit.DataStores.FileStores.Serialization.Yaml.Implementation
 {
     public class YamlSerializer : ISerializer
     {
-        readonly JsonSerializer _jsonSerializer = new JsonSerializer();
+        readonly JsonSerializer _jsonSerializer;
 
         readonly Serializer _serializer = new Serializer();
 
         readonly Deserializer _deserializer = new Deserializer();
 
+        public YamlSerializer()
+        {
+             _jsonSerializer = new JsonSerializer
+             {
+                 ContractResolver = new CamelCasePropertyNamesContractResolver()
+             };
+
+            _jsonSerializer.Converters.Add(new StringEnumConverter { CamelCaseText = true });
+        }
+
         public Task<TValue> Deserialize<TValue>(byte[] data)
         {
-            return Task.FromResult(JToken.FromObject(_deserializer.Deserialize<ExpandoObject>(
-                Encoding.UTF8.GetString(data, 0, data.Length))).ToObject<TValue>());
+            return Task.FromResult(JToken
+                .FromObject(
+                    _deserializer.Deserialize<ExpandoObject>(
+                        Encoding.UTF8.GetString(data, 0, data.Length)),
+                    _jsonSerializer)
+                .ToObject<TValue>());
         }
 
         public Task<byte[]> Serialize<TValue>(TValue value)
         {
             return Task.FromResult(Encoding.UTF8.GetBytes(_serializer.Serialize(
-                JToken.FromObject(value).ToObject<ExpandoObject>())));
+                JToken.FromObject(value, _jsonSerializer).ToObject<ExpandoObject>())));
         }
     }
 }
