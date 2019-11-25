62e7f9576bbaf6fec556077f9016b4c9b83b9dd5
Jim Noble <jim@halforbit.com>
2019-11-25 13:11:57 -0500

    restore legacy ftp impl

---
+++ Halforbit.DataStores/FileStores/LegacyFtp/Facets/UsernameAttribute.cs
@@ -0,0 +1,17 @@
+﻿using Halforbit.DataStores.FileStores.LegacyFtp.Implementation;
+using Halforbit.Facets.Attributes;
+using System;
+
+namespace Halforbit.DataStores.FileStores.LegacyFtp.Facets
+{
+    public class UsernameAttribute : FacetParameterAttribute
+    {
+        public UsernameAttribute(string value = null, string configKey = null) :
+            base(value, configKey)
+        { }
+
+        public override string ParameterName => "username";
+
+        public override Type TargetType => typeof(FtpFileStore);
+    }
+}
